08040655ba6c00d00676150a9e416886c0c1c7aa
Antoine <antoine.zambelli@gmail.com>
2021-12-21 01:49:10 -0800

    any update to dicts

--- ensemble_clustering/clustering.py
+++ ensemble_clustering/clustering.py
@@ -1,5 +1,6 @@
 from inspect import signature
 from typing import (
+    Any,
     Dict,
     List,
     Tuple,
@@ -37,7 +38,7 @@ class Clustering():
         self.algo_params = algo_params
         self.algo_metrics = algo_metrics
 
-    def get_hca_votes(self, h_params: Dict[str], algo: str) -> Dict[str, int]:
+    def get_hca_votes(self, h_params: Dict[Any], algo: str) -> Dict[str, int]:
         for _ in tqdm(range(1), desc=algo, ncols=100, leave=None):
             c_params = {k: (v if v else e) for k, v in self.algo_params[algo].items()}
             c_params.update(h_params)
@@ -55,7 +56,7 @@ class Clustering():
 
         return vote_dict
 
-    def run_trial(self, graph, h_params: Dict[str], n_c: int, algo: str):
+    def run_trial(self, graph, h_params: Dict[Any], n_c: int, algo: str):
         '''
         Generate labels for that n_c.
         '''
@@ -68,7 +69,7 @@ class Clustering():
 
         return model, labels
 
-    def compute_graph(self, h_params: Dict[str]):
+    def compute_graph(self, h_params: Dict[Any]):
         '''
         Compute nearest-neighbors graph if needed.
         '''
@@ -85,7 +86,7 @@ class Clustering():
 
         return graph
 
-    def get_votes(self, h_params: Dict[str], algo: str) -> Dict[str, int]:
+    def get_votes(self, h_params: Dict[Any], algo: str) -> Dict[str, int]:
         '''
         Loop through n_c and evaluate using metrics.
         '''
@@ -118,7 +119,7 @@ class Clustering():
 
         return vote_dict
 
-    def __call__(self, h_params: Dict[str], algo: str) -> Dict[str, int]:
+    def __call__(self, h_params: Dict[Any], algo: str) -> Dict[str, int]:
         '''
         Call for each hyperparameter combination and algo (clustering algorithm).
         '''
--- ensemble_clustering/matrix.py
+++ ensemble_clustering/matrix.py
@@ -10,14 +10,14 @@ import numpy as np
 
 
 class E():
-    def __init__(self, meta_res: List, param_perms: Dict[str, List[Dict[str]]]):
+    def __init__(self, meta_res: List, param_perms: Dict[str, List[Dict[Any]]]):
         '''
         On init, just build E according to mode or raw.
         '''
         self.meta_res = meta_res
         self.param_perms = param_perms
 
-    def get_best_algo(self, ground_truth: int, single: bool) -> List[Tuple[str, Dict[str]]]:
+    def get_best_algo(self, ground_truth: int, single: bool) -> List[Tuple[str, Dict[Any]]]:
         if ground_truth is None:
             return None
 
@@ -112,7 +112,7 @@ class E():
                         )
                     )
 
-    def __call__(self, e_params: Dict[str]) -> Dict[str]:
+    def __call__(self, e_params: Dict[Any]) -> Dict[Any]:
         E_res = {
             build: {
                 vote: {
