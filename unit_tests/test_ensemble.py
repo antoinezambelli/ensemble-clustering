5608a756f6936e525457039ec7794d42a559bc7e
Antoine <antoine.zambelli@gmail.com>
2021-12-22 04:51:51 -0800

    test fixes, coverage increase, utils tests

--- unit_tests/test_ensemble.py
+++ unit_tests/test_ensemble.py
@@ -38,7 +38,20 @@ def test_ensemble(algo_metrics_init, algo_params_init, h_params_init, param_perm
 def test_algo_selections(algo_metrics_init, algo_params_init, h_params_init, X_run, algo_selections_run):
     my_ensemble = Ensemble(algo_metrics_init, algo_params_init, h_params_init)
 
-    res, _ = my_ensemble(X_run, (2, 4), e_params=None, algo_selections=algo_selections_run)
+    res, nc = my_ensemble(X_run, (2, 4), e_params=None, algo_selections=algo_selections_run)
     assert len(res) == len(algo_selections_run)  # Test that we got all the keys.
     assert all(x in res for x in algo_selections_run)  # Test exact key match.
     assert all(v in res[k][0] for k in algo_selections_run for v in algo_metrics_init[k])  # Test that we got all metrics.
+    assert nc is None
+
+@pytest.mark.usefixtures('algo_metrics_init')
+@pytest.mark.usefixtures('algo_params_init')
+@pytest.mark.usefixtures('h_params_init')
+@pytest.mark.usefixtures('X_run')
+@pytest.mark.usefixtures('algo_selections_run')
+@pytest.mark.usefixtures('e_params_input')
+def test_e_params(algo_metrics_init, algo_params_init, h_params_init, X_run, algo_selections_run, e_params_input):
+    my_ensemble = Ensemble(algo_metrics_init, algo_params_init, h_params_init)
+
+    res, nc = my_ensemble(X_run, (2, 4), e_params=e_params_input, algo_selections=algo_selections_run)
+    assert nc is not None
--- unit_tests/test_utils.py
+++ unit_tests/test_utils.py
@@ -6,6 +6,8 @@ import pytest
 
 import numpy as np
 from fastcluster import linkage_vector
+from sklearn.cluster import MiniBatchKMeans
+from sklearn.mixture import GaussianMixture
 
 from ensemble_clustering.utils import (
     aic,
@@ -57,8 +59,31 @@ def test_hca_metrics(param_perms_output, X_run):
     res = hca_metrics(X_run, (2, 7), Z, 'silhouette_score')
     assert res == 5
 
-# TODO: need param perms and algo_params_init.
-# def test_aic():
-#     # Init a gaussian, a kmeans. Fit to X and run aic.
+@pytest.mark.usefixtures('X_run')
+@pytest.mark.usefixtures('param_perms_output')
+def test_aic_bic(X_run, param_perms_output):
+    gm = GaussianMixture(n_components=3, random_state=1, **param_perms_output['GaussianMixture'][0]).fit(X_run)
+    labels = gm.predict(X_run)
+    res_aic = aic(gm, X_run, 'GaussianMixture', labels)
+    res_bic = bic(gm, X_run, 'GaussianMixture', labels)
+    assert np.isclose(res_aic, 47.00303638131331)
+    assert np.isclose(res_bic, 50.33147240424781)
 
-#     assert True
+    km = MiniBatchKMeans(
+        n_clusters=3,
+        random_state=1,
+        compute_labels=True,
+        **param_perms_output['MiniBatchKMeans'][0]
+    ).fit(X_run)
+    labels = km.labels_
+    res_aic = aic(km, X_run, 'MiniBatchKMeans', labels)
+    res_bic = bic(km, X_run, 'MiniBatchKMeans', labels)
+    assert np.isclose(res_aic, 215.60766768110437)
+    assert np.isclose(res_bic, 218.33093351805078)
+
+@pytest.mark.usefixtures('X_run')
+def test_inertia(X_run):
+    labels = [1, 1, 1, 1, 1, 0, 0, 0, 0, 0]
+
+    res = inertia(X_run, labels)
+    assert np.isclose(res, 87.20440924993774)
